88995396c9cb76330e6f43032c905ae403098028
Comfort <comfort.prince.hluyo@gmail.com>
2024-04-11 17:13:54 +0200

    Completed building the Logo and Nav menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,22 @@
 import { logo } from '../../data/Constants'
+import navLinks from '../../data/navLinks'
 
 export default function Home() {
 	return(
 		<>
 			<header 
 				className="
-					md:mx-16 lg:mx-32 
+					md:mx-16 lg:mx-32
+					flex
+					border-b-2 border-light-grayish-blue
 				"
 			>
-				<Logo/>
-				<Nav/>
+				<div
+					className="flex justify-between gap-12"
+				>
+					<Logo/>
+					<Nav/>
+				</div>
 			</header>
 			<main>
 				
@@ -20,6 +27,35 @@ export default function Home() {
 
 function Logo() {
 	return(
-		<img src={logo} alt="Logo"/>
+		<span 
+			className="
+				inline-grid place-items-center
+				md:border-b-4 border-black border-opacity-0
+			"
+		>
+			<img src={logo} alt="Logo"/>
+		</span>
+	)
+}
+
+function Nav() {
+	return(
+		<nav
+			className="flex gap-6 max-md:hidden"
+		>
+			{navLinks.map((link) => (
+				<a 
+					href="#!"
+					className="
+						py-6 cursor-pointer text-sm
+						border-b-4 border-black border-opacity-0 hover:border-orange hover:border-opacity-100
+						text-dark-grayish-blue hover:text-very-dark-blue
+						transition-all duration-300
+					"
+				>
+					{link}
+				</a>
+			))}
+		</nav>
 	)
 }
